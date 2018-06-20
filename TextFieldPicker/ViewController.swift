6dffc1f5b51564c36b847c24598232f3159e4882
katafuchix <katafuchi@deskplate.net>
2018-06-20 13:02:37 +0900

    first commit

--- TextFieldPicker/ViewController.swift
+++ TextFieldPicker/ViewController.swift
@@ -8,13 +8,60 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,  UIPickerViewDelegate, UIPickerViewDataSource {
 
+    @IBOutlet weak var textField: UITextField!
+    
+    var pickerView: UIPickerView = UIPickerView()
+    let list = ["", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        pickerView.delegate = self
+        pickerView.dataSource = self
+        pickerView.showsSelectionIndicator = true
+        
+        let toolbar = UIToolbar(frame: CGRectMake(0, 0, 0, 35))
+        let doneItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(ViewController.done))
+        let cancelItem = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(ViewController.cancel))
+        toolbar.setItems([cancelItem, doneItem], animated: true)
+        
+        self.textField.inputView = pickerView
+        self.textField.inputAccessoryView = toolbar
     }
 
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return list.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return list[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        self.textField.text = list[row]
+    }
+    
+    @objc func cancel() {
+        self.textField.text = ""
+        self.textField.endEditing(true)
+    }
+    
+    @objc func done() {
+        self.textField.endEditing(true)
+    }
+    
+    func CGRectMake(_ x: CGFloat, _ y: CGFloat, _ width: CGFloat, _ height: CGFloat) -> CGRect {
+        return CGRect(x: x, y: y, width: width, height: height)
+    }
+
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
